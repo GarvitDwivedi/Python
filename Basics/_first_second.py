f3229b7de369d58e0cbea74636c3475664392b3d
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2025-01-27 12:43:42 +0530

    Done with internal details

---
+++ Basics/_first_second.py
@@ -0,0 +1,19 @@
+from _first import fun 
+
+fun("Second")
+
+
+
+# So now we have some question that waht is __pycahce__ folder and why it is been Created 
+
+# So the first step is the python program is compiled to byte code 
+# and then the python V.M execute the file 
+
+# So now why this __pycache polder is created so whenever we import someting then this folder is 
+# created and inside this folder we have a file __filename.pythoncompliertype-pythonVersion.pyc
+
+#code loop to iterate byte code and byte code is not a machine code --->> it is python specific iterpretation 
+#
+
+
+
--- Basics/first_second.py
+++
@@ -1,7 +0,0 @@
-from _first import fun 
-
-fun("Second")
-
-
-
-# So now we have some question that waht is __pycahce__ folder and why it is been Created 
