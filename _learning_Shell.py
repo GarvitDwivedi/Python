c2cd35bd9edde5c1ece3ebb4e1bd231fe475e370
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2025-01-29 22:29:24 +0530

    Leanining loop

--- _learning_Shell.py
+++ _learning_Shell.py
@@ -1,3 +1,10 @@
-print(5  * "Hello")
+# print(5  * "Hello")
 
 
+# if we want to use shell we can easliy 
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
