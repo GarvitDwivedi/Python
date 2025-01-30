b63f3f4441fd8112047a7ecf802ec01371c4cdd7
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2025-01-30 16:04:27 +0530

    Mutable vs Unmutalbe data type

---
+++ Basics/mutableandun.py
@@ -0,0 +1,28 @@
+username = "Hitesh" 
+
+print(username)
+
+
+username = "Jalal"
+
+print(username)
+
+
+#MUTABLE --> Mutable means the thing whose referse can be changed 
+# example -- list dict set string etc 
+# Unmutable --> Unmutable means the things whose value can be changed 
+# example --> int double flaot tuple etc 
+# 
+# 
+# 
+#  Strint usename is assigned to "Hitesh"
+#  And later it has been assigned to Jalal
+# So Usernamr -------->>> hitesh
+
+
+# So Usernamr --------______      hitesh
+#                          |
+# Later it assigned to      ------>> username 
+
+
+
