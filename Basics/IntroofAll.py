a668fdc7ec128c644c4e98a8500519ef2c92d215
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2025-02-01 23:48:41 +0530

    Leanining the basics

---
+++ Basics/IntroofAll.py
@@ -0,0 +1,35 @@
+username = "Hi" 
+
+#the data is not assigned to any data type but HI has the memory type 
+
+a = 1 
+
+b = a 
+
+a  = 10 
+print(a , b)
+
+
+#a is assigned to 1 then b is assigned to 1 then ref of a is changed but it is not going to impact b value
+
+
+name = "Garvit" 
+newname = name 
+
+print(name , newname) 
+
+name = "Heelo" 
+print(name , newname)
+
+t1 = (1,2,3,4)
+
+print(t1)
+
+t2 = t1 
+
+
+
+l1 = [1,2,3,4,5,6]
+
+print(len(l1))
+
