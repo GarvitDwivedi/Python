af443bdb400f1c8d339f764c190667387ac6b4ac
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2025-02-04 21:27:38 +0530

    String part complete

---
+++ Strings/string.py
@@ -0,0 +1,25 @@
+#SLICING 
+
+name = "Garvit Dwivedi"
+
+newname = name[::2]
+#starting index : ending index + 1 : jump / hop
+print(newname)
+
+
+#Working 
+print(name.upper())
+print(name.lower())
+
+print(name[1])
+print(name[-1])
+
+a = "Harivt"
+b = a    # This is not alllowed in python string is immutable we cannot change a particular value 
+print(a)
+print(b)
+
+s =  "Garvit is a champain and a kabbadi player "
+x = s.replace("a" , "and") 
+print(s)
+print(x)
