7a60d023746f9af6ce5333a7142e78d7f95be3dd
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2025-02-19 21:47:57 +0530

    Basics of Class

---
+++ classes/class.py
@@ -0,0 +1,15 @@
+class student:
+    def __init__(self , name , id):
+        self.name = name 
+        self.id = id 
+
+
+
+s1  = student("Garvit" , 29507)
+s2  = student("Gaurav" , 29094)
+
+print(s1.name)
+print(s1.id)
+        
+print(s2.name)
+print(s2.id)
