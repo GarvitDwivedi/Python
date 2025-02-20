8734e8bd6e87b10a45c808b31163cd6e2c83575a
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2025-02-20 23:40:05 +0530

    Classes and self

--- classes/class.py
+++ classes/class.py
@@ -2,7 +2,9 @@ class student:
     def __init__(self , name , id):
         self.name = name 
         self.id = id 
-
+    
+    def printDetail(self):
+        print(self.name , "---->" , self.id)
 
 
 s1  = student("Garvit" , 29507)
@@ -10,6 +12,9 @@ s2  = student("Gaurav" , 29094)
 
 print(s1.name)
 print(s1.id)
+(s1.printDetail())
         
+
+
 print(s2.name)
 print(s2.id)
